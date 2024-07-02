2fa4cf7e56d2529b0953b214b7284b2207a70c03
Salome <salomekaeni@gmail.com>
2024-07-02 02:52:25 -0700

    everything

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2;
+//=> 62
 
-// Write your code here
+let random = Math.floor(Math.random() * 1000) + 1;
+
+let num3 = 34;
+let num4 = 10;
+let mod = 4
+
+let max = Math.max(5, 10, 15, 20);
+//=> 20
